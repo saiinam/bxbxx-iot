0d425edff7aa016e47777cda07a4afdd6e68da9b
saiinam <71181573+saiinam@users.noreply.github.com>
2021-08-18 21:01:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
     <td><h2>RESULT</h2></td>
     <td><h2>LAST UPDATE</h2></td>
   </tr>
+  
   <tr>
     <td> <h3> Humidity </h3><br>
         <iframe frameborder="0" style="height:300px;width:500px"
         src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
     </td>
+    
     <td>
       <?php
          $Temperature = file_get_contents('https://api.thingspeak.com/channels/1458410/fields/1/last.txt');
@@ -26,18 +28,19 @@
         <br> <?php echo " Temperature : ".$Humidity ?> °C </h3>
     </td>
     </tr>
+  
     <tr>
         <td><h3> Temperature </h3><br>
         <iframe frameborder="0" style="height:300px;width:500px" 
         src="https://thingspeak.com/channels/1458410/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Temperature"></iframe>
     </td>
     </tr>
+  
     <tr>
     <td><h3> Location </h3><br>
         <iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/maps/channel_show" title="Location"></iframe>
     </td>
     </tr>
-
 
 </table>
  
