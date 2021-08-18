b74c21ee62cb3f4a57515a29866d00555de32fcb
saiinam <71181573+saiinam@users.noreply.github.com>
2021-08-18 18:18:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <h1> Name : Kotchakorn Somsanong <br> ID : 62100029 </h1>
-<iframe frameborder="0" style="height:300;width:500" src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
-<iframe frameborder="0" style="height:300;width:500" src="https://thingspeak.com/channels/1458410/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Temperaturet"></iframe>
-<iframe frameborder="0" style="height:300;width:500" src="https://thingspeak.com/channels/1458410/maps/channel_show" title="Location"></iframe>
+<iframe frameborder="0" style="height:500;width:500" src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
+<iframe frameborder="0" style="height:500;width:500" src="https://thingspeak.com/channels/1458410/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Temperaturet"></iframe>
+<iframe frameborder="0" style="height:500;width:500" src="https://thingspeak.com/channels/1458410/maps/channel_show" title="Location"></iframe>
