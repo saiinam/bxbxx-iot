8125dd17e5dd48e31812c9418d653bcd511177da
saiinam <71181573+saiinam@users.noreply.github.com>
2021-08-18 19:41:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,27 +5,25 @@
 <table style="width:100%">
   <tr>
     <td><h2>RESULT</td>
-    <td><h2>LAST UPDATE</td>		
-  </tr>
-  <tr>
-    <td> <h3> Humidity </h3><br>
-        <iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
-    </td>	
-    <td>
-      
-  <?php
-    $Temperature = file_get_contents('https://api.thingspeak.com/channels/1458410/fields/1/last.txt');
-    $Humidity = file_get_contents('https://api.thingspeak.com/channels/1458410/fields/2/last.txt');
-  ?>
-    <script type="text/JavaScript">
-      function timedRefresh(timeoutPeriod) {
-      setTimeout("location.reload(true);",timeoutPeriod);
-    }
+    <td><h2>LAST UPDATE</h2>
+      <?php
+         $Temperature = file_get_contents('https://api.thingspeak.com/channels/1458410/fields/1/last.txt');
+         $Humidity = file_get_contents('https://api.thingspeak.com/channels/1458410/fields/2/last.txt');
+      ?>
+         <script type="text/JavaScript">
+          function timedRefresh(timeoutPeriod) {
+          setTimeout("location.reload(true);",timeoutPeriod);
+        }
   </script>
   </script>
         <br> <?php echo "Humidity : ".$Temperature ?> % </br> 
         <br> <?php echo " Temperature : ".$Humidity ?> °C </br>
-   </td>
+  </td>		
+  </tr>
+  <tr>
+    <td> <h3> Humidity </h3><br>
+        <iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
+    </td>
   </tr>
 
   <tr>
