715f40c3bc80e8b6ef42a84c6f0c7e35e13ef9f0
saiinam <71181573+saiinam@users.noreply.github.com>
2021-08-18 17:57:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <h1> Name : Kotchakorn Somsanong <br> ID : 62100029 </h1>
 <iframe src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
 <iframe src="https://thingspeak.com/channels/1458410/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Temperaturet"></iframe>
-<iframe src="https://thingspeak.com/channels/1458410/maps/channel_show" style="height:100%;width:100%" title="Location"></iframe>
+<iframe src="https://thingspeak.com/channels/1458410/maps/channel_show" style="height:100%;width:50%" title="Location"></iframe>
