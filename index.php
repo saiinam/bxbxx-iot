59aa2f6965a0211cd8f37942f68d3fa207d5a1e8
saiinam <71181573+saiinam@users.noreply.github.com>
2021-08-18 19:24:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <body onload="JavaScript:timedRefresh(15000);"> 
   
 <h1> Name : Kotchakorn Somsanong <br> ID : 62100029 </h1>
-<iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
-<iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Temperaturet"></iframe>
+  
+<table style="width:100%">
+  <tr>
+    <td><h2>RESULT</td>
+    <td><h2>LAST UPDATE</td>		
+  </tr>
+  <tr>
+    <td> <h3> Humidity </h3><br>
+<iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe></td>	
+  </tr>
+
+  <tr>
+    <td><h3> Temperature </h3><br>
+<iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Temperature"></iframe>
+</td>
+  </tr>
+
+<tr>
+    <td><h3> Location </h3><br>
 <iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/maps/channel_show" title="Location"></iframe>
+</td>
+  </tr>
+</table>
 
  <?php
  $Temperature = file_get_contents('https://api.thingspeak.com/channels/1458410/fields/1/last.txt');
