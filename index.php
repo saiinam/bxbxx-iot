f57605b806afb7a790851b658582a7f648379bc2
saiinam <71181573+saiinam@users.noreply.github.com>
2021-08-18 20:49:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,22 +4,14 @@
   
 <table style=widht="100%" height="100%" align="center">
   <tr>
-    <td><h2>RESULT</td>
-  </td>		
+    <td><h2>RESULT</h2></td>
+    <td rowspan="2"><h2>LAST UPDATE</h2></td>
   </tr>
   <tr>
     <td> <h3> Humidity </h3><br>
         <iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Humidity"></iframe>
     </td>
-        <td><h3> Temperature </h3><br>
-        <iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Temperature"></iframe>
-    </td>
-    <td><h3> Location </h3><br>
-        <iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/maps/channel_show" title="Location"></iframe>
-    </td>
-    
-  </tr>
-<td>
+    <td>
       <?php
          $Temperature = file_get_contents('https://api.thingspeak.com/channels/1458410/fields/1/last.txt');
          $Humidity = file_get_contents('https://api.thingspeak.com/channels/1458410/fields/2/last.txt');
@@ -29,12 +21,21 @@
           setTimeout("location.reload(true);",timeoutPeriod);
         }
     </script>
-    </script>
-        <h2>LAST UPDATE</h2>
         <h3> <?php echo "Humidity : ".$Temperature ?> %
-        <br> <?php echo " Temperature : ".$Humidity ?> °C </h3></br>
+        <br> <?php echo " Temperature : ".$Humidity ?> °C </h3>
     </td>
-  </tr>
+    </tr>
+    <tr>
+        <td><h3> Temperature </h3><br>
+        <iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">"Temperature"></iframe>
+    </td>
+    </tr>
+    <tr>
+    <td><h3> Location </h3><br>
+        <iframe frameborder="0" style="height:300px;width:500px" src="https://thingspeak.com/channels/1458410/maps/channel_show" title="Location"></iframe>
+    </td>
+    </tr>
+
 
 </table>
  
