5db653b1bb516fae306dc7c2fa5e6b6d8c8d4aef
saiinam <71181573+saiinam@users.noreply.github.com>
2021-08-18 17:19:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
-Kotchakorn Somsanong
-https://thingspeak.com/channels/1458410/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15
-https://thingspeak.com/channels/1458410/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15
-https://thingspeak.com/channels/1458410/maps/channel_show
+<h1> Kotchakorn Somsanong </h>
